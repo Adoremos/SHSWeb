ba9358fed8322b24884221c731790d071087e6b4
minajuri <jurebancos@gbox.adnu.edu.ph>
2018-03-10 15:06:02 +0800

    added class students

--- proj/SHSWeb/admin.py
+++ proj/SHSWeb/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from SHSWeb.models import Students
 # Register your models here.
+
+admin.site.register(Students)
--- proj/SHSWeb/models.py
+++ proj/SHSWeb/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    name= models.CharField(max_length=1000,blank=True,null=False)
+    email= models.CharField(max_length=1000,blank=True,null=False)
