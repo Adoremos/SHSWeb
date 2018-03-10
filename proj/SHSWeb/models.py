f5fde0484238bc2ce9cba16634508b5917a27edb
minajuri <jurebancos@gbox.adnu.edu.ph>
2018-03-10 15:59:15 +0800

    implemented register page

--- proj/SHSWeb/models.py
+++ proj/SHSWeb/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Students(models.Model):
     name= models.CharField(max_length=1000,blank=True,null=False)
     email= models.CharField(max_length=1000,blank=True,null=False)
+    password=models.CharField(max_length=1000,blank=True,null=False)
+
+    def __str__(self):
+        return  self.name 
--- proj/SHSWeb/views.py
+++ proj/SHSWeb/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from SHSWeb.forms import studentform
+from SHSWeb.models import Students
 # Create your views here.
 def contact(request):
     
@@ -12,3 +14,13 @@ def about(request):
 def index(request):
 
     return render(request, "index.html", {})
+
+def register(request):
+    if request.POST: 
+        obj = Students()
+        obj.name = request.POST.get("name", '')
+        obj.email = request.POST.get("email", '')
+        obj.password = request.POST.get("password", '')
+        obj.save()
+    form=studentform()
+    return render(request, "register.html",{"form" : form})
